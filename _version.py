30469e3fd5d6ea70023d7172287d3fe910d13218
F33RNI <xxoinvizionxx@gmail.com>
2024-03-31 13:05:40 -0500

    build: update LlM-Api-Open

--- _version.py
+++ _version.py
@@ -20,7 +20,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 from packaging import version
 
-__version__ = "5.2.2"
+__version__ = "5.2.3"
 
 
 def version_major() -> int:
