18b334da327d5f3bc880b6ed69c0ceaae80dcf1d
F33RNI <xxoinvizionxx@gmail.com>
2024-03-31 01:32:52 -0500

    chore: add missing import

--- request_response_container.py
+++ request_response_container.py
@@ -18,7 +18,7 @@ You should have received a copy of the GNU Affero General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
-from typing import List
+from typing import List, Tuple
 
 from telegram import InlineKeyboardMarkup
 
